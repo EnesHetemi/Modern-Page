f1cfddd2483a8677738836b7a56d7eded9f4d35a
EnesHetemi <eneshetemi3@gmail.com>
2023-12-10 21:59:32 +0100

    Inicializimi i Projektit

---
+++ ModernWebParent/ModernFrontEnd/src/main/java/com/modern/site/ModernFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.modern.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernFrontEndApplication.class, args);
+	}
+
+}
