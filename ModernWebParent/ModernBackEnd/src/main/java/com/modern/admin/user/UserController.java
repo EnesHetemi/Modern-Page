0b3a5b02f7f3b6ae56abe66d346b7d2fcdaa3b39
EnesHetemi <eneshetemi3@gmail.com>
2024-01-09 15:50:45 +0100

    Shtimi i Ikonave, mundesia per te ndryshuar dhe fshir perdorues

--- ModernWebParent/ModernBackEnd/src/main/java/com/modern/admin/user/UserController.java
+++ ModernWebParent/ModernBackEnd/src/main/java/com/modern/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,6 +36,7 @@ public class UserController {
 
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Krijo përdorues të ri");
 
 		return "user_form";
 	}
@@ -45,6 +47,41 @@ public class UserController {
 		service.save(user);
 
 		redirectAttributes.addFlashAttribute("message", "Përdoruesi është ruajtur me sukses.");
+
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			User user = service.get(id);
+			List<Role> listRoles = service.listRoles();
+
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Ndrysho përdoruesin (ID: " + id + ")");
+			model.addAttribute("listRoles", listRoles);
+
+			return "user_form";
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+			return "redirect:/users";
+		}
+	}
+	
+	
+	@GetMapping("/users/delete/{id}")
+	public String deleteUser(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			service.delete(id);;
+			redirectAttributes.addFlashAttribute("message", 
+					"Përdoruesi me ID " + id + " është fshirë me sukses");
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+		}
 
 		return "redirect:/users";
 	}
---
+++ ModernWebParent/ModernBackEnd/src/main/java/com/modern/admin/user/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.modern.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
